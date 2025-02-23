7aec935d806ee8f6be4e813b502fd7029d9da3e2
Akash Madavi <akashmadavi2001@gmail.com>
2025-02-23 17:45:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+let message = document.getElementById('message');
+let str = document.getElementById('strenght');
+let password = document.getElementById('password');
+let inputbox = document.querySelector('.input-box');
+
+password.addEventListener('input', () => {
+    if (password.value.length > 0) {
+        message.style.display = "block";
+    }
+    else {
+        message.style.display = "none";
+    }
+
+    if (password.value.length <= 4) {
+        str.innerHTML = "weak";
+        inputbox.style.borderColor = "#ff0000";
+        message.style.color = "#ff0000";
+    }
+    else if (password.value.length <= 6) {
+        str.innerHTML = "medium";
+        inputbox.style.borderColor = "#ffff00";
+        message.style.color = "#ffff00";
+    }
+    else if (password.value.length <= 8) {
+        str.innerHTML = "strong";
+        inputbox.style.borderColor = "#99ff00";
+        message.style.color = "#99ff00";
+    }
+})
